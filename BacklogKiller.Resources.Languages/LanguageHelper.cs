ff65e92d0b41d5407c6954f8ddaf5ec3fdbaf9ca
Daniel Silva <daniel@teleconsistemas.com.br>
2020-01-03 11:08:22 -0300

    base project

---
+++ BacklogKiller.Resources.Languages/LanguageHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace BacklogKiller.Resources.Languages
+{
+    public class LanguageHelper
+    {
+        private readonly ResourceManager _resourceManager;
+        public CultureInfo _cultureInfo { get; private set; }
+
+
+        public LanguageHelper(CultureInfo cultureInfo)
+        {
+            _cultureInfo = cultureInfo;
+            var assembly = Assembly.GetExecutingAssembly();
+            _resourceManager = new ResourceManager(
+                baseName: "BacklogKiller.Resources.Languages.Resources.Resource",
+                assembly: assembly
+                );
+        }
+
+        public string GetString(Strings key)
+        {
+            return _resourceManager.GetString(key.ToString(), _cultureInfo);
+        }
+
+    }
+}
---
+++ BacklogKiller.Resources.Languages/Strings.cs
@@ -0,0 +1,17 @@
+﻿namespace BacklogKiller.Resources.Languages
+{
+    public enum Strings
+    {
+        ProjectRootDirectory,
+        Analyze,
+        Substitutions,
+        ToLocate,
+        ReplaceWith,
+        EmptyResult,
+        Alert,
+        Results,
+        NewFiles,
+        ModifiedFiles,
+        GenerateSelectedFiles
+    }
+}
